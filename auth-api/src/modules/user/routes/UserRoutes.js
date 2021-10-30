9c00ed4b5520e21fc43d099eee2e51d48ee2fb5d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-30 19:50:50 -0300

    028. Desenvolvendo a autenticação com JWT

--- auth-api/src/modules/user/routes/UserRoutes.js
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -6,4 +6,6 @@ const router = new Router()
 
 router.get('/api/user/email/:email', UserController.findByEmail)
 
+router.post('/api/user/auth', UserController.getAccessToken)
+
 export default router
