692176aa65ff18175f91efb4ec5503f2f4c8aedf
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-11-02 23:04:25 -0300

    054. Criando Sender da aplicação

--- sales-api/app.js
+++ sales-api/app.js
@@ -6,6 +6,8 @@ import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js'
 
 import checkToken from './src/config/auth/checkToken.js'
 
+import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabbitmq/productStockUpdateSender.js'
+
 const app = express()
 const env = process.env
 const PORT = env.PORT || 8082
@@ -14,7 +16,31 @@ connect()
 createInitialData()
 connectRabbitMq()
 
-app.use(checkToken)
+// app.use(checkToken)
+
+app.get('/test', async (req, res) => {
+  try {
+    sendMessageToProductStockUpdateQueue([
+      {
+        productId: 1001,
+        quantity: 4
+      },
+      {
+        productId: 1002,
+        quantity: 2
+      },
+      {
+        productId: 1003,
+        quantity: 3
+      }
+    ])
+
+    return res.status(200).json({ status: 200 })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: true })
+  }
+})
 
 app.get('/api/status', async (req, res) => {
   return res.status(200).json({
